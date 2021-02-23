04e5225af6ef878151a4fd192e3e7c85759d2dce
D00dleman <vbif1099@gmail.com>
2021-02-23 10:38:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ponchi",
-    version="0.2",
-    packages=["ponchi", "ponchi.clear_app"],
-    long_description="AAAAAA"
+    name='ponchi',
+    version='0.2',
+    author='D00dleman',
+    license='MIT',
+    description='Python Telegram bot framework',
+    url='https://github.com/D00dleman/ponchi'
+    packages=['ponchi', 'ponchi.clear_app'],
+    long_description=open('README.md').read(),
+    install_requires=[
+        'certifi==2020.12.5',
+        'chardet==4.0.0',
+        'idna==2.10',
+        'pony==0.7.14',
+        'PyMySQL==1.0.1',
+        'pyTelegramBotAPI==3.7.4',
+        'requests==2.25.1',
+        'urllib3==1.26.2',
+    ]
+
 )
